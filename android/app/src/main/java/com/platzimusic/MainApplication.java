5ecee4b12054ed8a6a01b543f31f82ad8930f72f
beto <jooseplugo@gmail.com>
2017-05-16 21:09:27 -0500

    Agregando login facebook para Android

--- android/app/src/main/java/com/platzimusic/MainApplication.java
+++ android/app/src/main/java/com/platzimusic/MainApplication.java
@@ -3,6 +3,7 @@ package com.platzimusic;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -11,6 +12,10 @@ import com.facebook.soloader.SoLoader;
 
 import java.util.Arrays;
 import java.util.List;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.appevents.AppEventsLogger;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -24,19 +29,28 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new VectorIconsPackage()
       );
     }
   };
+
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
 
   @Override
   public ReactNativeHost getReactNativeHost() {
     return mReactNativeHost;
   }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        // If you want to use AppEventsLogger to log events.
+        AppEventsLogger.activateApp(this);
+    }
 }
--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ import {
 
 import {Scene, Router} from 'react-native-router-flux';
 
+import LoginView from './LoginView';
 import HomeView from './HomeView';
 import ArtistDetailView from './ArtistDetailView';
 
@@ -16,6 +17,7 @@ export default class PlatziMusic extends Component {
     const isAndroid = Platform.OS === 'android';
 
     return <Router>
+      <Scene key="login" component={LoginView} hideNavBar/>
       <Scene key="root">
         <Scene key="home" component={HomeView} hideNavBar/>
         <Scene key="artistDetail" component={ArtistDetailView} hideNavBar={isAndroid}/>        
